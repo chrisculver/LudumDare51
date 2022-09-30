815726f48094a42884a50e5a9e09d18d725848fc
chrisculver <chris.cb.culver@gmail.com>
2022-09-30 18:27:12 +0100

    Making sure pausing works as expected

--- UI/pause_menu.gd
+++ UI/pause_menu.gd
@@ -1,18 +1,19 @@
-extends PopupPanel
+extends Control
 
 
 func _input(event):
 	if event.is_action_pressed("pause_menu"):
-		hide()
+		visible=false
 		get_tree().paused = false
+		get_tree().get_root().set_input_as_handled()
 
 
 func _on_resume_pressed():
-	hide()
+	visible=false
 	get_tree().paused = false
 
 
 func _on_quit_pressed():
-	hide()
+	visible=false
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://main_menu.tscn")
--- game.gd
+++ game.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+var playTime = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,10 +9,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	playTime += delta
+	$UILayer/Timer/Clock.text = ("elapsed: %02d" % playTime) + "s"
 
 
 func _input(event):
 	if event.is_action_pressed("pause_menu"):
-		$UILayer/PauseMenu.show()
+		$UILayer/PauseMenu.visible = true
 		get_tree().paused = true
+		#get_tree().get_root().set_input_as_handled()
